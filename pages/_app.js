abdf76b2bcc67d656fa571e554ace17f33f66003
afshakhanum <afshakhanstar@gmail.com>
2022-02-07 22:20:52 +0530

    Added search function

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import '../styles/globals.css'
 import './components/Poster/Poster.css'
 import './components/Header/Header.css'
-import './components/Input/Input.css'
 import './components/Movies/Movies.css'
 import './components/Popular -Movies/Popular.css'
 
--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,66 @@
 import Poster from './components/Poster/Poster'
 import Header from './components/Header/Header'
-import Input from './components/Input/Input'
 import Movies from './components/Movies/Movies'
 import styles from '../styles/Home.module.css'
 import Popular from './components/Popular -Movies/Popular'
 import React, { useState, useEffect } from "react";
+import Search from '@mui/icons-material/Search';
 
 export default function Home() {
-  const [data, setData] = useState([]);
+  const [movies, setData] = useState([]);
   const [Visible, setVisible] = useState(5);
-  const loadmore = () => {
+  const [search, setSearch] = useState('');
+  const img1 = "https://image.tmdb.org/t/p/w500" ;
+  
+  const loadmore = () => { 
     setVisible((Visible) => Visible+5)
   }
   useEffect(() => {
     moviePromise();
   }, []);
 
+  
   const moviePromise = () => {
     fetch("https://api.themoviedb.org/3/movie/popular?api_key=0122f66b835be1351367d17f60ca287b&language=en-US&page=1")
       .then((response) => response.json())
       .then((recievedData) => setData(recievedData.results));
   };
-  console.log(data);
- function mapping(){
-   return(
-    data.slice(0,Visible).map((item,id) => (
-      <Popular key={id} src={item.poster_path}/>
-    ))
-   )
- }
+  const result = () => {
+    if(search) {
+      const MyResult = movies.filter((movie) => {
+        return movie.title.toLowerCase().includes(search.toLowerCase());
+      });
+      return MyResult ? (
+        MyResult.map((movie) => 
+          <div key={movie.id} className="cards" >
+          <img className="movie-tiles" src={img1 + movie.poster_path} alt="" />
+          </div>
+      )
+      ):(
+        <h1>Results not found</h1>
+      )
+      }
+      else{
+        return movies.slice(0, Visible).map((movie) => 
+          <div key={movie.id} className="cards" >
+          <img className="movie-tiles" src={img1 + movie.poster_path} alt="" />
+          </div>
+      )
+    }
+}
+  console.log(movies);
+
   return (
     <div className={styles.container}>
      <Header />
      <Poster/>
-     <Input />
+     <div className="search-container" >
+        <Search className="btn" />
+       <input placeholder='Search movies and Click enter' onChange={(e) => setSearch(e.target.value)} />
+     </div>
      <Movies />
      <div className='movie'>
-     {mapping()}
+     {result()}
      </div>
       <div className="btn-container" >
       <button onClick={loadmore} className="btn1" >Load more</button>
