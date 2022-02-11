2cd8ccc9e1348ecdb8cca9312eacecdc6ee10299
afshakhanum <afshakhanstar@gmail.com>
2022-02-11 14:03:07 +0530

    error cleared

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
       }
       else{
         return movies.slice(0, Visible).map((movie,id) => 
-          <Link href={`/components/${movie.id}`} key={props.id}>
+          <Link href={`/components/${movie.id}`} key={movie.id}>
           <div key={id} className="cards" >
           <img className="movie-tiles" src={img1 + movie.poster_path} alt="" />
           </div>
