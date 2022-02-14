bfde9f1d941ed38edf370906ba08082d1c1fbec9
afshakhanum <afshakhanstar@gmail.com>
2022-02-14 12:05:38 +0530

    error fixed

--- pages/components/Poster/Poster.js
+++ pages/components/Poster/Poster.js
@@ -4,8 +4,8 @@ export default function Poster() {
       <div className="container" >
       <div className="img1">
          <div className="content">
-         <h1 className="MovName">The Matrix Resurrections</h1>
-          <p className="Descrptn">Plagued by strange memories, Neo's life takes an unexpected turn when he finds himself back inside the Matrix.</p>
+         <h1 className="MovName">The Matrix  Resurrections</h1>
+          <p className="Descrptn">Plagued by strange memories, Neo/'s life takes an unexpected turn when he finds himself back inside the Matrix.</p>
          </div>
       </div>
       </div>
